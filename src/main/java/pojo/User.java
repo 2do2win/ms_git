4094be73b4bab3fdbf80536d1cec0857a2b13f26
Adminstrator <2787298943@qq.com>
2019-08-11 09:47:05 +0800

    thirdcommit

--- src/main/java/pojo/User.java
+++ src/main/java/pojo/User.java
@@ -5,10 +5,6 @@ public class User {
     private String email;
 
 
-    public User(String name, String email) {
-        this.name = name;
-        this.email = email;
-    }
 
     public String getName() {
         return name;
